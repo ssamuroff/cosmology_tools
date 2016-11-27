d92be72d147506c1f8192f330860ae4186a023e2
Simon Samuroff <samuroff@fornax.jb.man.ac.uk>
2016-11-27 14:59:24 +0000

    calibrate_all now takes a config file

--- im3shape/calibrate_all.py
+++ im3shape/calibrate_all.py
@@ -5,52 +5,51 @@ import tools.diagnostics as di
 import tools.plots as pl
 import fitsio as fi
 import pylab as plt
-import os
-
-
-# Input im3shape results on data
-i3s_dir = "/share/des/disc7/samuroff/des/im3shape-v2-infocuts.fits" 
-# Input im3shape results on simulation
-hoopoe_dir="/share/des/disc6/samuroff/y1/hoopoe/y1_collated/"
-# Where to save the output plots and bias table
-plots_dir = "/home/samuroff/shear_pipeline/bias_calibration/v1.0/release/"
-table_dir ="/home/samuroff/shear_pipeline/bias_calibration/v1.0/nbc_data/"
-# The final calibrated catalogue -  the end result should just be the i3s_dir file saved with some extra columns
-output_catalogue = "/home/samuroff/shear_pipeline/bias_calibration/v1.0/y1a1-im3shape-r-1-2-2.fits"
-
+import os, yaml, argparse
 
 plt.switch_backend("agg")
 
-def main():
+parser = argparse.ArgumentParser(add_help=False)
+parser.add_argument('--config',"-c", type=str, action='store')
+parser.add_argument('--catalogue', action='store_true')
+parser.add_argument('--calculate', action='store_true')
+
+args = parser.parse_args()
+
+config = yaml.load(open(args.config))
+
+def main(args):
 	im3shape_columns = ["e1", "e2", "mean_psf_e1_sky", "mean_psf_e2_sky", "snr", "mean_rgpp_rp", "radius", "coadd_objects_id", "mean_flux", "n_exposure", "stamp_size", "info_flag", "is_bulge", "tilename"]
 	truth_columns = ['DES_id', 'cosmos_ident', 'cosmos_photoz', 'sextractor_pixel_offset', 'true_g1', 'true_g2', 'intrinsic_e1', 'intrinsic_e2', 'ra', 'dec', 'hlr', 'mag', 'flux']
+
 	# Load the y1 data
-	y1v2 = s.shapecat(res=i3s_dir)
-	y1v2.load(truth=False, prune=True, cols=[im3shape_columns,truth_columns])
-	y1v2.res=y1v2.res[y1v2.res["info_flag"]==0]
+	if args.calculate or args.catalogue:
+		y1v2 = s.shapecat(res=config["i3s_dir"])
+		y1v2.load(truth=False, prune=True, cols=[im3shape_columns,truth_columns])
+		y1v2.res=y1v2.res[y1v2.res["info_flag"]==0]
 	
 
 	# And the simulation results
-	hoopoe = s.shapecat(res="%s/results/bord-fits/main"%hoopoe_dir,truth="%s/truth"%hoopoe_dir)
-	hoopoe.load(truth=True, cols=[im3shape_columns,truth_columns])
-	#hoopoe.res=fi.FITS("/share/des/disc7/samuroff/des/hoopoe-y1a1-v02-bord.fits")["i3s"].read()
-	#hoopoe.truth=fi.FITS("/share/des/disc7/samuroff/des/hoopoe-y1a1-v02-bord.fits")["truth"].read()
-	sel=np.isfinite(hoopoe.res["mean_psf_e1_sky"]) & np.isfinite(hoopoe.res["mean_psf_e2_sky"])
-	hoopoe.res=hoopoe.res[sel]
-	hoopoe.truth=hoopoe.truth[sel]
-	
-	
+	if args.calculate:
+		hoopoe = s.shapecat(res="%s/bord-fits/main"%config["hoopoe_dir"] ,truth="%s/truth"%config["hoopoe_dir"])
+		hoopoe.load(truth=True, cols=[im3shape_columns,truth_columns])
+		#hoopoe.res=fi.FITS("/share/des/disc7/samuroff/des/hoopoe-y1a1-v02-bord.fits")["i3s"].read()
+		#hoopoe.truth=fi.FITS("/share/des/disc7/samuroff/des/hoopoe-y1a1-v02-bord.fits")["truth"].read()
+		sel=np.isfinite(hoopoe.res["mean_psf_e1_sky"]) & np.isfinite(hoopoe.res["mean_psf_e2_sky"])
+		hoopoe.res=hoopoe.res[sel]
+		hoopoe.truth=hoopoe.truth[sel]
 
-	diagnostics(y1v2, hoopoe, rbf=True)
-	diagnostics(y1v2, hoopoe, histograms=False, alpha=False, table=False, rbf=False)
+		diagnostics(y1v2, hoopoe, vssnr=config["output"]["snr"], vsredshift=config["output"]["redshift"], table=config["output"]["tables"], alpha=config["output"]["alpha"], histograms=config["output"]["histograms"], rbf=True)
+		diagnostics(y1v2, hoopoe, histograms=False, alpha=False, table=False, rbf=False)
 
-	calibrate(y1v2)
+	if args.catalogue:
+		calibrate(y1v2)
 
 def mkdirs():
-	os.system("mkdir -p %s/inputs"%plots_dir)
-	os.system("mkdir -p %s/outputs"%plots_dir)
-	os.system("mkdir -p %s/rbf"%plots_dir)
-	os.system("mkdir -p %s"%table_dir)
+	os.system("mkdir -p %s/release/inputs"%config["output_dir"] )
+	os.system("mkdir -p %s/release/outputs"%config["output_dir"] )
+	os.system("mkdir -p %s/release/rbf"%config["output_dir"] )
+	os.system("mkdir -p %s/nbc_data"%config["output_dir"] )
 
 
 def calibrate(data, method="rbf"):
@@ -60,26 +59,29 @@ def calibrate(data, method="rbf"):
 	# Set up the wrappers
 	nbc = cal.nbc()
 	nbc_disc = cal.nbc()
-	nbc_bulge = cal.nbc()
-	nbc.res = data.res
 	nbc_disc.res = data.res
-	nbc_bulge.res = data.res
-
-
+	nbc_bulge = cal.nbc()
+	
 	# Fit or interpolate
 	if method is "polynomial":
-		nbc_disc.fit("m", table="%s/bias_table_hoopoe-v1-fullcat-disc.fits"%table_dir)
-		nbc_disc.fit("a", table="%s/bias_table_hoopoe-v1-fullcat-disc.fits"%table_dir)
-		nbc_bulge.fit("m", table="%s/bias_table_hoopoe-v1-fullcat-bulge.fits"%table_dir)
-		nbc_bulge.fit("a", table="%s/bias_table_hoopoe-v1-fullcat-bulge.fits"%table_dir)
+		nbc_disc.fit("m", table="%s/nbc_data/bias_table_hoopoe-v1-fullcat-disc.fits"%config["output_dir"])
+		nbc_disc.fit("a", table="%s/nbc_data/bias_table_hoopoe-v1-fullcat-disc.fits"%config["output_dir"])
+		nbc_bulge.fit("m", table="%s/nbc_data/bias_table_hoopoe-v1-fullcat-bulge.fits"%config["output_dir"])
+		nbc_bulge.fit("a", table="%s/nbc_data/bias_table_hoopoe-v1-fullcat-bulge.fits"%config["output_dir"])
 	elif method is "rbf":
-		nbc_disc.fit_rbf(table="%s/bias_table_hoopoe-v1-halfcat-disc.fits"%table_dir)
-		nbc_bulge.fit_rbf(table="%s/bias_table_hoopoe-v1-halfcat-bulge.fits"%table_dir)
+		nbc_disc.fit_rbf(table="%s/nbc_data/bias_table_hoopoe-v1-halfcat-disc.fits"%config["output_dir"])
+		nbc_bulge.fit_rbf(table="%s/nbc_data/bias_table_hoopoe-v1-halfcat-bulge.fits"%config["output_dir"])
 
 	# Apply to get a bias correction for each galaxy in the data
 	nbc_disc.apply(split_half=0, use_rbf=rbf)
+	nbc.res = nbc_disc.res
+	del(nbc_disc.res)
+
+	nbc_bulge.res = data.res
 	nbc_bulge.apply(split_half=0, use_rbf=rbf)
-	nbc.get_combined_calibration(nbc_disc,nbc_bulge, split_half=0)
+	nbc.res[nbc.res["is_bulge"].astype(bool)] = nbc_bulge.res[nbc.res["is_bulge"].astype(bool)]
+	del(nbc_bulge.res)
+
 
 	if not os.path.exists(os.path.dirname(output_catalogue)):
 		os.system("mkdir -p %s"%os.path.dirname(output_catalogue))
@@ -89,13 +91,13 @@ def calibrate(data, method="rbf"):
 
 def diagnostics(y1v2, hoopoe, histograms=True, alpha=True, table=True, vssnr=True, vsredshift=True, rbf=True):
 
-	if not os.path.exists(plots_dir):
+	if not os.path.exists(config["output_dir"]):
 		print "Warning - output plots directory does not exist"
 
 	# First make some diagnostic distributions of the observable parameters relevant for the NBC
 	if histograms:
-		pl.histograms_vs_input(["e", "size", "flux"], hoopoe.truth, data2=y1v2.res, outdir="%s/inputs"%plots_dir)
-		pl.histograms(["snr", "e", "size", "rgpp", "flux", "psfe", "psf_size"], hoopoe.res, data2=y1v2.res, outdir="%s/outputs"%plots_dir)
+		pl.histograms_vs_input(["e", "size", "flux"], hoopoe.truth, data2=y1v2.res, outdir="%s/release/inputs"%config["output_dir"])
+		pl.histograms(["snr", "e", "size", "rgpp", "flux", "psfe", "psf_size"], hoopoe.res, data2=y1v2.res, outdir="%s/release/outputs"%config["output_dir"])
 
 	if alpha:
 		# Global fit of alpha to compare with previous runs
@@ -104,8 +106,8 @@ def diagnostics(y1v2, hoopoe, histograms=True, alpha=True, table=True, vssnr=Tru
 		plt.subplots_adjust(wspace=0, hspace=0, top=0.95, bottom=0.1)
 		plt.title("Y1 v1 Sim, PSF v01")
 
-		os.system("mkdir -p %s/alpha"%plots_dir)
-		plt.savefig("%s/alpha/alphaplot-e-vs-epsf-linfit.png"%plots_dir)
+		os.system("mkdir -p %s/release/alpha"%config["output_dir"])
+		plt.savefig("%s/release/alpha/alphaplot-e-vs-epsf-linfit.png"%config["output_dir"])
 		plt.close()
 
 
@@ -127,32 +129,32 @@ def diagnostics(y1v2, hoopoe, histograms=True, alpha=True, table=True, vssnr=Tru
 
 	# Fit the binned galaxies. Save one set of calibration data for disc objects, one for bulges
 	if table:
-		nbc_disc.compute(split_half=1, fit="disc", rbins=10, sbins=10, rlim=(0.9,4.5), slim=(10,200), table_name="%s/bias_table_hoopoe-v1-halfcat-disc.fits"%table_dir)
-		nbc_bulge.compute(split_half=1, fit="bulge", rbins=10, sbins=10, rlim=(0.9,4.5), slim=(10,200), table_name="%s/bias_table_hoopoe-v1-halfcat-bulge.fits"%table_dir)
+		nbc_disc.compute(split_half=1, fit="disc", rbins=10, sbins=10, rlim=(0.9,4.5), slim=(10,200), table_name="%s/nbc_data/bias_table_hoopoe-v1-halfcat-disc.fits"%config["output_dir"])
+		nbc_bulge.compute(split_half=1, fit="bulge", rbins=10, sbins=10, rlim=(0.9,4.5), slim=(10,200), table_name="%s/nbc_data/bias_table_hoopoe-v1-halfcat-bulge.fits"%config["output_dir"])
 	
 	if rbf:
-		nbc_disc.fit_rbf(table="%s/bias_table_hoopoe-v1-halfcat-disc.fits"%table_dir)
-		nbc_bulge.fit_rbf(table="%s/bias_table_hoopoe-v1-halfcat-bulge.fits"%table_dir)
+		nbc_disc.fit_rbf(table="%s/nbc_data/bias_table_hoopoe-v1-halfcat-disc.fits"%config["output_dir"])
+		nbc_bulge.fit_rbf(table="%s/nbc_data/bias_table_hoopoe-v1-halfcat-bulge.fits"%config["output_dir"])
 	else:
-		nbc_disc.fit("m", table="%s/bias_table_hoopoe-v1-halfcat-disc.fits"%table_dir)
-		nbc_disc.fit("a", table="%s/bias_table_hoopoe-v1-halfcat-disc.fits"%table_dir)
-		nbc_bulge.fit("m", table="%s/bias_table_hoopoe-v1-halfcat-bulge.fits"%table_dir)
-		nbc_bulge.fit("a", table="%s/bias_table_hoopoe-v1-halfcat-bulge.fits"%table_dir)
+		nbc_disc.fit("m", table="%s/nbc_data/bias_table_hoopoe-v1-halfcat-disc.fits"%config["output_dir"])
+		nbc_disc.fit("a", table="%s/nbc_data/bias_table_hoopoe-v1-halfcat-disc.fits"%config["output_dir"])
+		nbc_bulge.fit("m", table="%s/nbc_data/bias_table_hoopoe-v1-halfcat-bulge.fits"%config["output_dir"])
+		nbc_bulge.fit("a", table="%s/nbc_data/bias_table_hoopoe-v1-halfcat-bulge.fits"%config["output_dir"])
 	
 
 	# Now plot out the points and the resulting smooth fit
 	if vssnr:
-		nbc_disc.bias_fit_vs_pts(table="%s/bias_table_hoopoe-v1-halfcat-disc.fits"%table_dir, bias_name="m", do_half=1, output="%s/m-vs-snr-disc-v1-1.png"%(plots_dir+"/rbf"*rbf), use_rbf=rbf)
-		nbc_disc.bias_fit_vs_pts(table="%s/bias_table_hoopoe-v1-halfcat-disc.fits"%table_dir, bias_name="a", do_half=1, output="%s/alpha-vs-snr-disc-v1-1.png"%(plots_dir+"/rbf"*rbf), use_rbf=rbf)
+		nbc_disc.bias_fit_vs_pts(table="%s/nbc_data/bias_table_hoopoe-v1-halfcat-disc.fits"%config["output_dir"], bias_name="m", do_half=1, output="%s/m-vs-snr-disc-v1-1.png"%(config["output_dir"]+"/release"+"/rbf"*rbf), use_rbf=rbf)
+		nbc_disc.bias_fit_vs_pts(table="%s/nbc_data/bias_table_hoopoe-v1-halfcat-disc.fits"%config["output_dir"], bias_name="a", do_half=1, output="%s/alpha-vs-snr-disc-v1-1.png"%(config["output_dir"]+"/release"+"/rbf"*rbf), use_rbf=rbf)
 		plt.close()
-		nbc_disc.bias_fit_vs_pts(table="%s/bias_table_hoopoe-v1-halfcat-disc.fits"%table_dir, bias_name="m", do_half=2, output="%s/m-vs-snr-disc-v1-2.png"%(plots_dir+"/rbf"*rbf), use_rbf=rbf)
-		nbc_disc.bias_fit_vs_pts(table="%s/bias_table_hoopoe-v1-halfcat-disc.fits"%table_dir, bias_name="a", do_half=2, output="%s/alpha-vs-snr-disc-v1-2.png"%(plots_dir+"/rbf"*rbf), use_rbf=rbf)
+		nbc_disc.bias_fit_vs_pts(table="%s/nbc_data/bias_table_hoopoe-v1-halfcat-disc.fits"%config["output_dir"], bias_name="m", do_half=2, output="%s/m-vs-snr-disc-v1-2.png"%(config["output_dir"]+"/release"+"/rbf"*rbf), use_rbf=rbf)
+		nbc_disc.bias_fit_vs_pts(table="%s/nbc_data/bias_table_hoopoe-v1-halfcat-disc.fits"%config["output_dir"], bias_name="a", do_half=2, output="%s/alpha-vs-snr-disc-v1-2.png"%(config["output_dir"]+"release"+"/rbf"*rbf), use_rbf=rbf)
 		plt.close()
-		nbc_bulge.bias_fit_vs_pts(table="%s/bias_table_hoopoe-v1-halfcat-bulge.fits"%table_dir, bias_name="m", do_half=1, output="%s/m-vs-snr-bulge-v1-2.png"%(plots_dir+"/rbf"*rbf), use_rbf=rbf)
-		nbc_bulge.bias_fit_vs_pts(table="%s/bias_table_hoopoe-v1-halfcat-bulge.fits"%table_dir, bias_name="a", do_half=1, output="%s/alpha-vs-snr-bulge-v1-1.png"%(plots_dir+"/rbf"*rbf), use_rbf=rbf)
+		nbc_bulge.bias_fit_vs_pts(table="%s/nbc_data/bias_table_hoopoe-v1-halfcat-bulge.fits"%config["output_dir"], bias_name="m", do_half=1, output="%s/m-vs-snr-bulge-v1-2.png"%(config["output_dir"]+"/release"+"/rbf"*rbf), use_rbf=rbf)
+		nbc_bulge.bias_fit_vs_pts(table="%s/nbc_data/bias_table_hoopoe-v1-halfcat-bulge.fits"%config["output_dir"], bias_name="a", do_half=1, output="%s/alpha-vs-snr-bulge-v1-1.png"%(config["output_dir"]+"/release"+"/rbf"*rbf), use_rbf=rbf)
 		plt.close()
-		nbc_bulge.bias_fit_vs_pts(table="%s/bias_table_hoopoe-v1-halfcat-bulge.fits"%table_dir, bias_name="m", do_half=2, output="%s/m-vs-snr-bulge-v1-1.png"%(plots_dir+"/rbf"*rbf), use_rbf=rbf)
-		nbc_bulge.bias_fit_vs_pts(table="%s/bias_table_hoopoe-v1-halfcat-bulge.fits"%table_dir, bias_name="a", do_half=2, output="%s/alpha-vs-snr-bulge-v1-2.png"%(plots_dir+"/rbf"*rbf), use_rbf=rbf)
+		nbc_bulge.bias_fit_vs_pts(table="%s/nbc_data/bias_table_hoopoe-v1-halfcat-bulge.fits"%config["output_dir"], bias_name="m", do_half=2, output="%s/m-vs-snr-bulge-v1-1.png"%(config["output_dir"]+"/release"+"/rbf"*rbf), use_rbf=rbf)
+		nbc_bulge.bias_fit_vs_pts(table="%s/nbc_data/bias_table_hoopoe-v1-halfcat-bulge.fits"%config["output_dir"], bias_name="a", do_half=2, output="%s/alpha-vs-snr-bulge-v1-2.png"%(config["output_dir"]+"/release"+"/rbf"*rbf), use_rbf=rbf)
 		plt.close()
 
 	# Apply to the other half
@@ -167,14 +169,14 @@ def diagnostics(y1v2, hoopoe, histograms=True, alpha=True, table=True, vssnr=Tru
 		nbc.redshift_diagnostic(bias="m", label="Calibrated", ls="none", nbins=3, fmt=["^",">"], apply_calibration=True, colour="purple")
 		plt.ylabel("Multiplicative Bias $m$")
 		plt.legend(loc="lower left")
-		plt.savefig("%s/m-vs-redshift-diagnostic-v1.png"%(plots_dir+rbf*"/rbf"))
+		plt.savefig("%s/release/%s/m-vs-redshift-diagnostic-v1.png"%(config["output_dir"],rbf*"/rbf"))
 		plt.close()
 
 		nbc.redshift_diagnostic(bias="alpha", label="Uncalibrated",ls="none", nbins=3, fmt=["o","D"], colour="steelblue")
 		nbc.redshift_diagnostic(bias="alpha", label="Calibrated", ls="none",fmt=["^",">"], nbins=3, apply_calibration=True, colour="purple")
 		plt.ylabel(r"PSF Leakage $\alpha$")
 		plt.legend(loc="lower right")
-		plt.savefig("%s/alpha-vs-redshift-diagnostic-v1.png"%(plots_dir+rbf*"/rbf"))
+		plt.savefig("%s/release/%s/alpha-vs-redshift-diagnostic-v1.png"%(config["output_dir"],rbf*"/rbf"))
 		plt.close()
 
 
@@ -186,5 +188,5 @@ def diagnostics(y1v2, hoopoe, histograms=True, alpha=True, table=True, vssnr=Tru
 
 
 mkdirs()
-main()
+main(args)
 
