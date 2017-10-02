5e0109bae1b648e3feaf38a69e518e5e4d6bd42f
Simon Samuroff <samuroff@fornax.jb.man.ac.uk>
2017-10-02 01:01:31 +0100

    tweak plotting fn

--- colours.py
+++ colours.py
@@ -372,19 +372,19 @@ def colour_panels(shapes, pz):
 		mask[select & colour_select_r] = 1
 		colour_select_b = ((r-z)<lin)
 		mask[select & colour_select_b] = 2
-		plt.subplot(int("%d%d%d"%(4,1,b+1)), aspect=0.75)
+		plt.subplot(int("%d%d%d"%(4,1,b+1)), aspect=0.4)
 		y = r-i
 		x = i-z
 		colour_diagram(x[select & (mask==1)],y[select & (mask==1)], ylim=[0,2.5],xlim=[-0.2,1.3], split_param=[0,np.inf], colour='red' )
 		colour_diagram(x[select & (mask==2)],y[select & (mask==2)], ylim=[0,2.5],xlim=[-0.2,1.3], split_param=[0,np.inf], colour='royalblue' )
 		if (b==3):
-			plt.xlabel("$i-z$", fontsize=14)
+			plt.xlabel("$i-z$", fontsize=12)
 			plt.xticks([0,0.4,0.8,1.2])
 		else:
 			plt.xlabel("")
 			plt.xticks(visible=False)
-		plt.xticks(fontsize=14)
-		plt.ylabel("$r-i$", fontsize=14)
+		plt.xticks(fontsize=12)
+		plt.ylabel("$r-i$", fontsize=12)
 
 	return mask
 
