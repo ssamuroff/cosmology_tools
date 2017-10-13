69439334b51afc22fd3e03e281f5674f58768871
Simon Samuroff <samuroff@fornax.jb.man.ac.uk>
2017-10-13 21:25:35 +0100

    HSC data handling tool

---
+++ hsc.py
@@ -0,0 +1,29 @@
+import numpy as np
+import fitsio as fi
+import os
+
+
+class dr1:
+	def __init__(self, data=''):
+		print 'Initialised HSC DR1 data wrapper.'
+		if data=='':
+			data = '/global/cscratch1/sd/sws/hsc/pdr1_udeep_wide_depth_best'
+
+		self.base = data
+		print 'will obtain data from %s'%self.base
+
+		self.data = {}
+		return 0
+	def load_coadd(self, band='r', pointing='(8,7)'):
+		path = '%s/deepCoadd/HSC-%c/9813/%s'%(self.base,band.upper(),pointing)
+		full_path = '%s/calexp-HSC-%c-9813-%s.fits.gz'%(path, band.upper(), pointing)
+		filename = os.path.basename(full_path)
+
+		if filename not in self.data.keys():
+			self.data[filename]={}
+
+		self.data[filename]['coadd'] = fi.FITS(filename)
+		return 0
+
+
+
