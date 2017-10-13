b021279f3ffd7a10e3ad3b08fc0e7f7ff8ad5fea
Simon Samuroff <samuroff@fornax.jb.man.ac.uk>
2017-10-13 21:37:51 +0100

    HSC data handling tool

--- hsc.py
+++ hsc.py
@@ -25,5 +25,22 @@ class dr1:
 		self.data[filename]['coadd'] = fi.FITS(filename)
 		return 0
 
+	def detect(self, band='r', pointing='(8,7)', config='', weights=True):
+		path = '%s/deepCoadd/HSC-%c/9813/%s'%(self.base,band.upper(),pointing)
+		full_path = '%s/calexp-HSC-%c-9813-%s.fits.gz'%(path, band.upper(), pointing)
+		filename = os.path.basename(full_path)
+
+		print full_path
+
+		if config=='':
+			config='%s/seconfig-nersc'%self.base
+
+		template = 'sex %s/deepCoadd/HSC-%c/9813/%s/calexp-HSC-%c-9813-%s.fits.gz -c %s'%(self.base, band.upper(), pointing, band.upper(), pointing, config)
+
+		if weights:
+			template += "-WEIGHT_IMAGE %s'[1]'"%filename
+
+		print 'Done'
+
 
 
