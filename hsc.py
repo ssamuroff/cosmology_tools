6b821241e1070340adff17a86289a0bfdbf37ab7
Simon Samuroff <samuroff@fornax.jb.man.ac.uk>
2017-10-16 20:44:57 +0100

    testing stamp collection

--- hsc.py
+++ hsc.py
@@ -78,10 +78,10 @@ class dr1:
 		for b in bands:
 			for p in patches:
 
-				path = '%s/deepCoadd/HSC-%c/9813/%s'%(self.base,b.upper(),patch)
-				cat_path = '%s/calexp-HSC-%c-9813-%s_cat.fits'%(path, b.upper(), patch)
-				seg_path = '%s/calexp-HSC-%c-9813-%s_seg.fits'%(path, b.upper(), patch)
-				coadd_path = '%s/calexp-HSC-%c-9813-%s.fits.fz'%(path, b.upper(), patch)
+				path = '%s/deepCoadd/HSC-%c/9813/%s'%(self.base,b.upper(),p)
+				cat_path = '%s/calexp-HSC-%c-9813-%s_cat.fits'%(path, b.upper(), p)
+				seg_path = '%s/calexp-HSC-%c-9813-%s_seg.fits'%(path, b.upper(), p)
+				coadd_path = '%s/calexp-HSC-%c-9813-%s.fits.fz'%(path, b.upper(), p)
 				filename = os.path.basename(full_path)
 
 				print full_path
