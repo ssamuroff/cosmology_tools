2b1175b57993a6b408b16257dfd427f83deb3f87
Simon Samuroff <samuroff@fornax.jb.man.ac.uk>
2017-10-18 22:06:46 +0100

    conservative mode

--- hsc.py
+++ hsc.py
@@ -167,7 +167,7 @@ class dr1:
 				    edge_pixels = np.hstack((final[0,:], final[-1,:], final[:,0], final[:,-1]))
 				    sig_edge = np.std(edge_pixels)
 
-				    if sig_edge>1.:
+				    if sig_edge>0.5:
 				    	outdat['EDGE_FLAGS'][i]=1
 
 				    if (np.unique(seg_final).size>2):
