1ab1500986684d9b40a3dbeb76e0d596061b163e
Simon Samuroff <samuroff@fornax.jb.man.ac.uk>
2017-10-18 17:35:56 +0100

    hsc.py

--- hsc.py
+++ hsc.py
@@ -84,7 +84,7 @@ class dr1:
 			out_path2 = 'calexp-HSC-%c-9813_galsim_catalogue.fits'%(b.upper())
 			outfile2 = fi.FITS(out_path2, 'rw')
 
-			outdat_all = np.empty(50000, dtype=[('IDENT', int),('RA', float), ('DEC', float), ('GAL_FILENAME', 'S100'), ('GAL_HDU', int)])
+			outdat_all = np.empty(900000, dtype=[('IDENT', int),('RA', float), ('DEC', float), ('GAL_FILENAME', 'S100'), ('GAL_HDU', int)])
 			start=0
 
 			for ip,p in enumerate(patches):
@@ -169,7 +169,7 @@ class dr1:
 				    igal+=1
 				    ihdu+=1
 
-				import pdb ; pdb.set_trace()
+				#import pdb ; pdb.set_trace()
 
 				for name in outdat_all.dtype.names:
 					outdat_all[name][start:start+outdat[name].size] = outdat[name]
